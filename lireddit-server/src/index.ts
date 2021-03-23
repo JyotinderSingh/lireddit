768816172874dad6a8ba272c541085635db67996
Jyotinder Singh <jyotinder.singh@cloudera.com>
2021-03-24 00:11:22 +0530

    Iniital commit - CRUD

---
+++ lireddit-server/src/index.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  // This runs the migrations before doing anything else, so that we don't need to do that manually on the terminal
+  orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
+};
+
+main().catch((err) => console.error(err));
