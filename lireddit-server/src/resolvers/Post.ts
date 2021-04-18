546e93a52f09e6003449409769cf9647bd08e659
Jyotinder Singh <jyotinder.singh@cloudera.com>
2021-04-18 21:05:36 +0530

    minor bug fix

--- lireddit-server/src/resolvers/Post.ts
+++ lireddit-server/src/resolvers/Post.ts
@@ -136,10 +136,17 @@ export class PostResolver {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    const replacements: any[] = [realLimitPlusOne, req.session.userId];
+    const replacements: any[] = [realLimitPlusOne];
+
+    if (req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
 
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
 
     const posts = await getConnection().query(
@@ -155,11 +162,11 @@ export class PostResolver {
     ${
       req.session.userId
         ? ',(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
-        : 'null as "voteStatus'
+        : 'null as "voteStatus"'
     }
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."createdAt" < $3` : ""}
+    ${cursor ? `where p."createdAt" < $${cursorIdx}` : ""}
     order by p."createdAt" DESC
     limit $1
     `,
